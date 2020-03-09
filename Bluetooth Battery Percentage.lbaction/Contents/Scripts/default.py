052fd2ff5203312fbda3c761ae975d5e552e2be2
Yue <36361620+caoyu-yiyue@users.noreply.github.com>
2020-03-09 16:36:46 +0800

    Upload Battery Percentage Action

---
+++ Bluetooth Battery Percentage.lbaction/Contents/Scripts/default.py
@@ -0,0 +1,34 @@
+#!/usr/local/opt/python/libexec/bin/python
+#
+# LaunchBar Action Script
+#
+import json
+import sh
+import re
+
+try:
+    result = str(
+        sh.egrep(sh.ioreg('-r', '-l', '-n', 'AppleHSBluetoothDevice'),
+                 r'"BatteryPercent" = |^  \|   "Bluetooth Product Name" = '))
+except sh.ErrorReturnCode:
+    print('None Bluetooth Connecting.')
+    quit()
+
+names = [
+    match.group(1) for match in re.finditer(r'Product Name" = "(.+?)"', result)
+]
+BatteryPercent_nums = [
+    match.group(1) for match in re.finditer(r'Percent" = (\d+)', result)
+]
+
+name_percent_pairs = [
+    name + " : " + bp + "%" for name in names for bp in BatteryPercent_nums
+]
+
+items = []
+for pair in name_percent_pairs:
+    item = {}
+    item['title'] = pair
+    items.append(item)
+
+print(json.dumps(items))
