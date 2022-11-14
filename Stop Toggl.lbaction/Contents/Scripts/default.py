79034474f7f01cd2c6307347df88c6e03aa49bc9
caoyu-yiyue <Cy1093662798@outlook.com>
2022-11-14 16:12:53 +0800

    Update Stop Toggl for API v9, using only standard python libs.

--- Stop Toggl.lbaction/Contents/Scripts/default.py
+++ Stop Toggl.lbaction/Contents/Scripts/default.py
@@ -1,25 +1,52 @@
 #!/usr/bin/env python
 #
-# LaunchBar Action Script
+# LaunchBar Action Script for Stoping Running Toggl Entry.
 #
-from toggl.TogglPy import Toggl
-from requests import HTTPError
-import datetime
 
 # %%
-toggl = Toggl()
-# fill your own E-Mail and Password.
-toggl.setAuthCredentials('<EMAIL>', '<PASSWORD>')
+from base64 import b64encode
+from datetime import timedelta
+from urllib.error import URLError
+
+import requests
 
 # %%
+# Fill your API token
+TOKEN = '<Your API Token>'
+
+HEADERS = {
+    'content-type':
+    'application/json',
+    'Authorization':
+    'Basic %s' % b64encode(f"{TOKEN}:api_token".encode()).decode("ascii")
+}
+
+# %%
+# Get current running entry.
 try:
-    currentTimer = toggl.currentRunningTimeEntry()
-    if currentTimer['data'] is None:
-        print('No Time Entry Runnig Now.')
+    current_resp = requests.get(
+        'https://api.track.toggl.com/api/v9/me/time_entries/current',
+        headers=HEADERS)
+    current_entry = current_resp.json()
+
+    # if there's entry running, the stop it.
+    if current_entry is not None:
+        workspace_id = current_entry['workspace_id']
+        time_entry_id = current_entry['id']
+
+        stopped_resp = requests.patch(
+            (f'https://api.track.toggl.com/api/v9/workspaces/{workspace_id}/'
+             f'time_entries/{time_entry_id}/stop'),
+            headers=HEADERS)
+        stopped_entry = stopped_resp.json()
+
+        duration_str = str(timedelta(0, stopped_entry['duration']))
+        desc = stopped_entry['description']
+
+        print(f'Stopped {desc}, Have Run {duration_str}.')
+    # If no running entry, return.
     else:
-        stoppedEntry = toggl.stopTimeEntry(currentTimer['data']['id'])['data']
-        duration_str = str(datetime.timedelta(0, stoppedEntry['duration']))
-        print('Stopped: {}, Have Run {}.'.format(stoppedEntry['description'],
-                                                 duration_str))
-except HTTPError as e:
-    print(e)
+        print('No Time Entry Runnig Now.')
+
+except URLError:
+    print("Internet Error: Can't connent internet.")
